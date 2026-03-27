6734ef21957f953bff3b97760ecec47f131a478f
Jitu <jitudenis@gmail.com>
2026-03-27 16:23:16 +0200

    app.ts solved

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,6 @@
 import { Hono } from "hono";
-import { logger } from "hono/logger";
-
+import {logger} from 'hono/logger'
 const app = new Hono().basePath("/api").use(logger());
-
 (globalThis as any).app = app;
-
-app.get("/", (c) => {
-	return c.text("OK");
-});
 
 export default app;
