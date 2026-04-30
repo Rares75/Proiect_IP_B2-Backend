e4143de36a80ce9b964d69c45025c464005d7e92
diana139 <dianarosuu18@gmail.com>
2026-04-30 21:27:19 +0300

    restore InteractionsService to develop version

--- src/services/InteractionsService.ts
+++ src/services/InteractionsService.ts
@@ -1,4 +1,3 @@
-import { rateLimitSchema } from "better-auth";
 import { InteractionsRepository } from "../db/repositories/interactions.repository";
 import { inject } from "../di";
 import { Service } from "../di/decorators/service";
@@ -71,7 +70,7 @@ export class InteractionsService {
 					interaction.taskAssignmentId,
 				);
 				const receivedRating =
-					ratings.find((rating) => rating.receivedByUserId == userId) ?? null;
+					ratings.find((rating) => rating.receivedByUserId === userId) ?? null;
 				return {
 					interactionId: interaction.id,
 					taskAssignmentId: interaction.taskAssignmentId,
