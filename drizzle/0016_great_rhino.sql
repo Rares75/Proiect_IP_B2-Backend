598f22e2a24dee2a51459cd5e60a41fe5da9a764
ilinca06 <iancuilinca99@gmail.com>
2026-05-15 14:39:02 +0300

    [BE1-50] update 14:40

--- drizzle/0016_great_rhino.sql
+++ drizzle/0016_great_rhino.sql
@@ -1,4 +1,3 @@
-ALTER TABLE "messages" ALTER COLUMN "sender_id" DROP NOT NULL;--> statement-breakpoint
+
 ALTER TABLE "notifications" ALTER COLUMN "user_id" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "messages" ADD COLUMN "guest_session_id" varchar(128);--> statement-breakpoint
 ALTER TABLE "notifications" ADD COLUMN "guest_session_id" text;
