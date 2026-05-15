4fd2c80245903334d6238acda46acc9c557ced17
EmyLungu <lunguemy19@gmail.com>
2026-05-16 01:00:27 +0300

    limit 200

--- src/app.ts
+++ src/app.ts
@@ -23,8 +23,8 @@ const app = new Hono<AppEnv>().basePath("/api").use(
 app.use(
 	"*",
 	rateLimiter({
-		windowMs: 1 * 60 * 1000, // Max 150 Request-uri per minut
-		limit: 150,
+		windowMs: 1 * 60 * 1000, // Max 200 Request-uri per minut
+		limit: 200,
 		keyGenerator: (c) => c.req.header("x-forwarded-for") ?? "unknown",
 	}),
 );
